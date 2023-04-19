2cb2c7c363e59b3bb0f04a30ba9c793f828455cd
bBravee <jakub.kusmierczyk@op.pl>
2023-04-19 17:16:59 +0200

    first tests

---
+++ src/components/ukladSloneczny/Circle.js
@@ -0,0 +1,13 @@
+import './Circle.css';
+
+function Circle() {
+    return (
+        <div className="circle">
+            <span className='planet planet1'></span>
+            <span className='planet planet2'></span>
+            <span className='planet planet3'></span>
+        </div>
+    );
+}
+
+export default Circle;
