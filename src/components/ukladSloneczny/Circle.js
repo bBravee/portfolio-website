2dc968fd2062be03edf75bcfd3034f8f9b51b4b3
bBravee <jakub.kusmierczyk@op.pl>
2023-04-25 21:36:40 +0200

    circle test

--- src/components/ukladSloneczny/Circle.js
+++ src/components/ukladSloneczny/Circle.js
@@ -2,11 +2,23 @@ import './Circle.css';
 
 function Circle() {
     return (
-        <div className="circle">
-            <span className='planet planet1'></span>
-            <span className='planet planet2'></span>
-            <span className='planet planet3'></span>
-        </div>
+        <>
+            <div className="circle">
+              <div className="js-container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/2048px-Unofficial_JavaScript_logo_2.svg.png" alt="icon"/>
+              </div>
+              <div className="npm-container">
+                <img src="https://authy.com/wp-content/uploads/npm-logo.png" alt="icon"/>
+              </div>
+              <div className="react-container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" alt="icon"/>
+              </div>
+              <div className="git-container">
+                <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="icon"/>
+                <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="icon"/>
+              </div>
+            </div>
+        </>
     );
 }
 
---
+++ src/components/ukladSloneczny/ExampleCircle.js
@@ -0,0 +1,6 @@
+{/* <div class="circle">
+  <div class="slice1" style="background-color: red;"></div>
+  <div class="slice2" style="background-color: green;"></div>
+  <div class="slice" style="background-color: blue;"></div>
+  <div class="slice" style="background-color: yellow;"></div>
+</div> */}
