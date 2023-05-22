a111ea36855cbc5e0d33c11a9dbebe3fb06c6210
bBravee <jakub.kusmierczyk@op.pl>
2023-05-23 00:09:06 +0200

    project item animations

--- src/components/Projects/ProjectItem/ProjectItem.js
+++ src/components/Projects/ProjectItem/ProjectItem.js
@@ -2,24 +2,23 @@ import { Col, Row, Container } from "react-bootstrap";
 
 import './ProjectItem.css';
 
-
-
 function ProjectItem({ imgUrl, title, description, height }) {
+
     const containerStyles = {
         backgroundImage: `url(${imgUrl})`,
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
         backgroundSize: 'cover',
-        borderRadius: '16',    
-        boxShadow: '0 4 30 rgba(0, 0, 0, 0.1)'
+        borderRadius: '16px',
+        boxShadow: '0 4 30 rgba(0, 0, 0, 0.1)',
     };
-    
+
+
     return (
-        <Container style={containerStyles}>
+        <Container style={containerStyles} className="project-container">
+            <div className="overlay"></div>
             <Row style={{ height: height }} className="project-item-row">
-                <Col lg={4}>
-                </Col>
-                <Col lg={8}>
+                <Col lg={8} className="project-item-column">
                     <h1>{title}</h1>
                     <p>{description}</p>
                 </Col>
---
+++ src/data/projectsData.js
@@ -0,0 +1,63 @@
+const projectsData = [
+    [
+        {
+            title: "Weather App",
+            description: "An application that allows you to check the current weather.",
+            badges: [
+                "react",
+                "axios",
+                "api"
+            ],
+            imgUrl: "https://images.unsplash.com/photo-1682685795557-976f03aca7b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+            height: 400
+        },
+        {
+            title: "Workout App",
+            description: "An application that aims to help people with their training.",
+            badges: [
+                "react-native",
+                "expo",
+                "api"
+            ],
+            imgUrl: "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1642&q=80",
+            height: 300
+        },
+    ],
+    [
+        {
+            title: "Workout App",
+            description: "An application that aims to help people with their training.",
+            badges: [
+                "react-native",
+                "expo",
+                "api"
+            ],
+            imgUrl: "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1642&q=80",
+            height: 300
+        },
+        {
+            title: "Weather App",
+            description: "An application that allows you to check the current weather.",
+            badges: [
+                "react",
+                "axios",
+                "api"
+            ],
+            imgUrl: "https://images.unsplash.com/photo-1682685795557-976f03aca7b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80",
+            height: 380
+        },
+        {
+            title: "Workout App",
+            description: "An application that aims to help people with their training.",
+            badges: [
+                "react-native",
+                "expo",
+                "api"
+            ],
+            imgUrl: "https://images.unsplash.com/photo-1434725039720-aaad6dd32dfe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1642&q=80",
+            height: 360
+        },
+    ]
+];
+
+export default projectsData;
